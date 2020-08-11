133504f109b3eb8e0adf5d2fb543822e8f447538
eswaraluri <eswar.aluri@outlook.com>
2020-08-11 12:48:10 +0530

    first commit

---
+++ dotnet-sqldb-tutorial-master/dbo.Todoes.data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT [dbo].[Todoes] ON
+INSERT INTO [dbo].[Todoes] ([ID], [Description], [CreatedDate]) VALUES (1, N'eswar', N'2020-08-10 00:00:00')
+INSERT INTO [dbo].[Todoes] ([ID], [Description], [CreatedDate]) VALUES (2, N'rakesh', N'2020-08-10 00:00:00')
+INSERT INTO [dbo].[Todoes] ([ID], [Description], [CreatedDate]) VALUES (3, N'mounika', N'2020-08-10 00:00:00')
+INSERT INTO [dbo].[Todoes] ([ID], [Description], [CreatedDate]) VALUES (4, N'sdp', N'2020-08-10 00:00:00')
+INSERT INTO [dbo].[Todoes] ([ID], [Description], [CreatedDate]) VALUES (5, N'baji', N'2020-08-10 00:00:00')
+INSERT INTO [dbo].[Todoes] ([ID], [Description], [CreatedDate]) VALUES (5, N'cooldude', N'2020-08-10 00:00:00')
+SET IDENTITY_INSERT [dbo].[Todoes] OFF
